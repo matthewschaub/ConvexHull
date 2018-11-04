7686d58a63e4d159daaf1fdc1ea01b609c3a176f
MattSchaub <matthew.e.schaub@gmail.com>
2018-11-04 15:14:37 -0500

    Buf fix for argument parameters

--- convexHull.cpp
+++ convexHull.cpp
@@ -29,30 +29,35 @@ int main(int argc, char *argv[])
 {
    //Second project starts here
 	//grahamScan();
-	jarvisMarch(); 
+	//jarvisMarch(); 
 
 
 
-   if (argc < 3) 
+   if (argc < 3) {
       std::cout << "wrong format! should be \"a.exe algType dataFile\"";
+    }
    else {
-      std::string algType = argv[2];
-      std::string dataFilename = argv[3];
+      std::string algType = argv[1];
+      std::string dataFilename = argv[2];
       std::string outputFile = "";
       //read your data points from dataFile (see class example for the format)
       
       if (algType[0]=='G') {
-         //call your Graham Scan algorithm to solve the problem
-         outputFile = "hull_G.txt";
-		} 
-		else if (algType[0]=='J') {
-		         //call your Javis March algorithm to solve the problem
-		         outputFile = "hull_J.txt";
-		}
-		else { //default 
-		         //call your Quickhull algorithm to solve the problem
-		         outputFile = "hull_Q.txt";
-		   }
+      //call your Graham Scan algorithm to solve the problem
+      	outputFile = "hull_G.txt";
+         
+
+			} 
+			else if (algType[0]=='J') {
+		  //call your Javis March algorithm to solve the problem
+		  	outputFile = "hull_J.txt";
+		        
+			}
+			else { //default 
+		  //call your Quickhull algorithm to solve the problem
+		  	outputFile = "hull_Q.txt";
+
+		  }
 		}
       
       //write your convex hull to the outputFile (see class example for the format)
